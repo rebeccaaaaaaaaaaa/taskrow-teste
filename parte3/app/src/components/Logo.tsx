f2c3d56bdb2edeb958ef9f347821a04ae7a8a5ef
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-11-01 23:31:25 -0300

    feat: resolt lint erros for build to prod

--- parte3/app/src/components/Logo.tsx
+++ parte3/app/src/components/Logo.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Logo = () => {
   const svgStyle = {
     fill: '#5c76c5'
--- parte3/app/src/pages/DetalhesGrupo.tsx
+++ parte3/app/src/pages/DetalhesGrupo.tsx
@@ -1,14 +1,15 @@
 import { useParams, Outlet } from "react-router-dom";
 import { useGlobal } from "../hooks/useGlobal";
 import { Box, Container, Flex, Heading, List, ListItem } from "@chakra-ui/react";
+import { Grupo } from "../context/Grupos";
 
-function findGroupById(groups, idGrupo) {
+function findGroupById(groups: Grupo[], idGrupo: string) : unknown | null {
   for (const group of groups) {
     if (group.idGrupo === parseInt(idGrupo, 10)) {
       return group;
     }
     if (group.subGrupos) {
-      const nestedGroup = findGroupById(group.subGrupos, idGrupo);
+      const nestedGroup : unknown | null = findGroupById(group.subGrupos, idGrupo);
       if (nestedGroup) {
         return nestedGroup;
       }
@@ -16,6 +17,7 @@ function findGroupById(groups, idGrupo) {
   }
   return null;
 }
+
 
 function DetalhesGrupo() {
   const { idGrupo } = useParams();
@@ -25,7 +27,12 @@ function DetalhesGrupo() {
     return <div>Parâmetro ID ausente.</div>;
   }
 
-  const grupo = findGroupById(grupos, idGrupo);
+  interface Grupo {
+    nome: string;
+    usuarios: { idUsuario: number; nome: string }[];
+  }
+
+  const grupo = findGroupById(grupos, idGrupo) as Grupo;
 
   if (!grupo) {
     return (
@@ -37,28 +44,30 @@ function DetalhesGrupo() {
     );
   }
 
+
   return (
     <Container maxW="8xl">
-      <Flex background="#eee" p={10} width={"100%"} mt={20} gap={10}>
-        <Box>
-          <Heading fontWeight='normal'>
-            Detalhes do Grupo: <strong>{grupo.nome}</strong>
-          </Heading>
-          <p> Usuários pertencentes ao grupo: </p>
-          {grupo.usuarios && grupo.usuarios.length > 0 ? (
-            <List p={3}>
-              {grupo.usuarios.map((usuario) => (
-                <ListItem key={usuario.idUsuario}>- {usuario.nome}</ListItem>
-              ))}
-            </List>
-          ) : (
-            <p>Nenhum usuário cadastrado neste grupo.</p>
-          )}
-        </Box>
-        {/* Adicione mais informações do grupo aqui */}
-        <Outlet />
-      </Flex>
-    </Container>
+    <Flex background="#eee" p={10} width={"100%"} mt={20} gap={10}>
+      <Box>
+        <Heading fontWeight='normal'>
+          Detalhes do Grupo: <strong>{grupo.nome}</strong>
+        </Heading>
+        <p> Usuários pertencentes ao grupo: </p>
+        {grupo.usuarios && grupo.usuarios.length > 0 ? (
+          <List p={3}>
+            {grupo.usuarios.map((usuario) => (  // Removed the extra '}'
+              <ListItem key={usuario.idUsuario}>- {usuario.nome}</ListItem>
+            ))}
+          </List>
+        ) : (
+          <p>Nenhum usuário cadastrado neste grupo.</p>
+        )}
+      </Box>
+      {/* Adicione mais informações do grupo aqui */}
+      <Outlet />
+    </Flex>
+  </Container>
+  
   );
 }
 
